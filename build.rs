7931da4d3dd79507a4469fa684e495c95df2abae
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-07-03 07:19:52 +0200

    Simplify the build script

--- build.rs
+++ build.rs
@@ -1,14 +1,8 @@
 extern crate gcc;
 extern crate pkg_config;
 
-#[cfg(not(feature = "bundled"))]
 fn main() {
-    if pkg_config::find_library("sqlite3").is_err() {
+    if cfg!(feature = "bundled") || pkg_config::find_library("sqlite3").is_err() {
         gcc::compile_library("libsqlite3.a", &["source/sqlite3.c"]);
     }
-}
-
-#[cfg(feature = "bundled")]
-fn main() {
-    gcc::compile_library("libsqlite3.a", &["source/sqlite3.c"]);
 }
