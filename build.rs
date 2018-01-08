3dc1046ae292a3c009227251309c7b70f3ec2d47
Daniel Mueller <deso@posteo.net>
2018-01-08 12:10:31 -0800

    Update cc create to 1.0
    The gcc crate has been renamed to cc to make it more generically
    applicable. Also, it is now available as a 1.0 release. This change
    adjusts the library to use the new version of the cc crate in version
    1.0.

--- build.rs
+++ build.rs
@@ -1,8 +1,10 @@
-extern crate gcc;
+extern crate cc;
 extern crate pkg_config;
 
 fn main() {
     if cfg!(feature = "bundled") || pkg_config::find_library("sqlite3").is_err() {
-        gcc::compile_library("libsqlite3.a", &["source/sqlite3.c"]);
+        cc::Build::new()
+            .file("source/sqlite3.c")
+            .compile("libsqlite3.a");
     }
 }
