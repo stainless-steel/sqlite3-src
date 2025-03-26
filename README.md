259296f68fdab7e25533f41a3c2b30e8931b3a51
Sondre Vincent Russvoll <srussvoll@users.noreply.github.com>
2025-03-26 12:59:35 +0100

    Added WASM support.

--- README.md
+++ README.md
@@ -11,6 +11,18 @@ It is also possible to enable various [compile-time options] by setting
 environment variables with the same names, such as
 `SQLITE_ENABLE_FTS5`.
 
+## WASM support
+This package supports WASM through WASI.
+If you compile for the `wasip1` or `wasip2` targets all you need to do is define either 
+
+* the `sqlite3_src_wasi_sdk_path` configuration flag in rustc, or
+* the `WASI_SDK_PATH` environment variable
+
+to point to an installation of the [WASI SDK].
+
+If you are compiling for `wasm32-unknown-unknown` you also need to set the `sqlite3_src_wasi_env`
+configuration flag in rustc to specify which WASI version you want to compile for (e.g. `p1`).
+
 
 ## Contribution
 
@@ -20,6 +32,7 @@ will be licensed according to the terms given in [LICENSE.md](LICENSE.md).
 
 [SQLite]: https://sqlite.org
 [compile-time options]: https://www.sqlite.org/compile.html
+[WASI SDK]: https://github.com/WebAssembly/wasi-sdk/releases
 
 [build-img]: https://github.com/stainless-steel/sqlite3-src/workflows/build/badge.svg
 [build-url]: https://github.com/stainless-steel/sqlite3-src/actions/workflows/build.yml
--- build.rs
+++ build.rs
@@ -1,6 +1,9 @@
-use std::env;
+use std::{env, path::Path};
 
 fn main() {
+    let target_family = env::var("CARGO_CFG_TARGET_FAMILY").unwrap();
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
+
     if cfg!(feature = "bundled") || pkg_config::find_library("sqlite3").is_err() {
         let mut build = cc::Build::new();
         build.file("source/sqlite3.c");
@@ -9,6 +12,26 @@ fn main() {
                 build.define(&name, value.as_str());
             }
         }
+
+        if target_family == "wasm" {
+            let wasi_sdk_env = env::var("CARGO_CFG_SQLITE3_SRC_WASI_SDK_PATH")
+                .or(env::var("WASI_SDK_PATH"))
+                .unwrap();
+            let wasi_sdk = Path::new(&wasi_sdk_env).canonicalize().unwrap();
+            build.compiler(wasi_sdk.join("bin/clang"));
+            if target_os != "wasi" {
+                let Ok(wasi_env) = env::var("CARGO_CFG_SQLITE3_SRC_WASI_ENV") else {
+                    println!("cargo::error=Could not detect WASI environment. Please set the sqlite3_src_wasi_env config flag (e.g. \"p1\").");
+                    return;
+                };
+                build.target(&format!("wasm32-wasi{wasi_env}"));
+            }
+            build.define("__wasi__", None);
+            build.define("SQLITE_OMIT_LOAD_EXTENSION", "1");
+            build.define("SQLITE_THREADSAFE", "0");
+            build.flag("-Wno-unused");
+        }
+
         build.compile("libsqlite3.a");
     }
 }
